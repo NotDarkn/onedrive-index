d9ea90bbd27e3bef370743b50851d82568a0742a
Darkn <privateservice000@gmail.com>
2024-08-13 20:02:38 -0400

    feat: cloudflare pages ssr

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 const { i18n } = require('./next-i18next.config')
+export const runtime = "edge";
 
 module.exports = {
   i18n,
   reactStrictMode: true,
   // Required by Next i18n with API routes, otherwise API routes 404 when fetching without trailing slash
   trailingSlash: true,
+}
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
 }
